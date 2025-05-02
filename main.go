20bc9fe0eabd4417c5eebb290db61f6cdc635816
Rendhi <rendirendi11122@gmail.comcls>
2025-05-02 17:36:06 +0700

    meanmpilkan data user

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
-	daftar()
+	masukanDataDummy()
+	fmt.Println("=== Menampilkan User Terdaftar ===")
+	for i := 0; i < JumlahTerdaftar; i++ {
+		fmt.Println("id:", DatabasePengguna[i].UserId)
+		fmt.Println("nama:", DatabasePengguna[i].Nama)
+		fmt.Println("email:", DatabasePengguna[i].Email)
+		fmt.Println("")
+	}
+
+	fmt.Println("=== Menampilkan Informasi Startup ===")
+	for i := 0; i < JumlahTerdaftar; i++ {
+
+	}
 }
