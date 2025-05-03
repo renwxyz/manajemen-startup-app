ed67d650cb285377127aa45bbbd9ea79cb8f1d61
Rendhi <rendirendi11122@gmail.comcls>
2025-05-03 08:36:54 +0700

    Menyelesaikan fitur daftar

--- autentikasi.go
+++ autentikasi.go
@@ -3,9 +3,29 @@ package main
 import "fmt"
 
 func daftar() {
-	fmt.Print("on development")
+	var (
+		nama          string
+		email         string
+		kataSandi     string
+		jenisPengguna string
+	)
+
+	fmt.Println("===== DAFTAR =====")
+	fmt.Print("Nama :")
+	fmt.Scanln(&nama)
+	fmt.Print("Email :")
+	fmt.Scanln(&email)
+	fmt.Print("Kata sandi: ")
+	fmt.Scanln(&kataSandi)
+	fmt.Print("Jenis Pengguna [pendiri/investor] :")
+	fmt.Scanln(&jenisPengguna)
+
+	DatabasePengguna[JumlahTerdaftar].Nama = nama
+	DatabasePengguna[JumlahTerdaftar].Email = email
+	DatabasePengguna[JumlahTerdaftar].KataSandi = kataSandi
+	DatabasePengguna[JumlahTerdaftar].JenisPengguna = jenisPengguna
 }
 
 func masuk() {
-	fmt.Print("on development")
+	fmt.Println("on development")
 }
